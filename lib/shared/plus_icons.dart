c5b88fa951e681854b582b4966915470b9b085bf
zeldaababil19 <zeldaababil01@gmail.com>
2021-12-21 00:11:21 +0700

    user setting

--- lib/shared/plus_icons.dart
+++ lib/shared/plus_icons.dart
@@ -10,4 +10,5 @@ class Plus {
   static const IconData calendar_check_o = IconData(0xf274, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData history = IconData(0xe994, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData user = IconData(0xebab, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData jekel = IconData(0xebab, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
--- lib/utils/util.dart
+++ lib/utils/util.dart
@@ -1,9 +1,11 @@
 import 'package:capstone_final/models/model.dart';
+import 'package:capstone_final/shared/shared.dart';
 import 'package:capstone_final/ui/ui.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'dart:io' show Platform;
 
@@ -11,3 +13,4 @@ part 'auth_util.dart';
 part 'enum_util.dart';
 part 'secret.dart';
 part 'storage.dart';
+
